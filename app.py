841baa28404f4cf281ea2e2a9126b99889745761
vamsi <vamsiy2001@gmail.com>
2023-02-12 15:03:47 +0530

    simple frontend webapp is ready

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def home():
     return render_template('home.html')
 
 # predict api which we test in postman 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
     data = request.json['data']
     print(data)
     # print(type(data))
@@ -30,6 +30,14 @@ def predict():
     output = regmodel.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)
+    return render_template("home.html",prediction_text='The predicted house price would be {}'.format(output[0]))
 
 if __name__ == '__main__':
     app.run(debug=True)
